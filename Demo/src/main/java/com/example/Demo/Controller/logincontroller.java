b43beab73849049f3afdb8d73eb65e5e7843e621
bharadwajnithin <110156210+bharadwajnithin@users.noreply.github.com>
2022-12-08 02:28:25 +1200

    Add files via upload

---
+++ Demo/src/main/java/com/example/Demo/Controller/logincontroller.java
@@ -0,0 +1,28 @@
+package com.example.Demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class logincontroller {
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String loginpage() {
+		return "login";
+	}
+	@RequestMapping(value = "/login", method= RequestMethod.POST)
+	 public String welcomepage(ModelMap model, @RequestParam String userid, @RequestParam String password ) {
+		if(userid.equals("admin") && password.equals("nroot")) {
+			model.put("userid", userid);
+			return "welcome";
+		}
+		
+		
+		model.put("errorMsg", "Please enter correct Details");
+		return "login";
+	}
+	
+
+}
